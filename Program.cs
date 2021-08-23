f293a2bd93f2ef4c3f986169eafca9ab4df38cbb
Adam Batson <tibatson@gmail.com>
2021-08-23 17:54:48 -0500

    Add comment with project requirements

--- Program.cs
+++ Program.cs
@@ -8,5 +8,23 @@ namespace Calculator
         {
             Console.WriteLine("Hello World!");
         }
+
+        /*
+         ***Project objectives***
+        Create a console application
+        Manage a git repository containing a C# console application
+        Demonstrate 'branch' workflow
+    
+        ***Project requirement***    
+        Make a minimum of six commits.
+        Include a .gitignore file that removes the .suo from source control
+        Follow csharp code conventions
+        Follow the four outlined steps above. You may choose to do more, like ask for three numbers if you finish the project quickly
+        Implement a form of branching, i.e. if/else or switch statement
+        Validate user input and catch an exception. i.e. if you request a number from the user, and they instead type out a word like writing "four," request they resubmit or exit the program gracefully." You may want to review training on exception handling and reach out to the class for additional exception handling information. We will discuss exception handling more next week.
+        Try to think of additional exception you may catch that could arise from your code.
+        You may demonstrate objected oriented programming through making additional classes. Additional classes are not a requirement this week.
+       */
+
     }
 }
